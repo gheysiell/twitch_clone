ca803d00b2f404ba37aa69184e403a02002d4487
gheysiell <gheysiellfarukzinn@hotmail.com>
2022-10-09 17:51:01 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# twitch_clone
+
+<div> <h2> Mobile clone of twitch, made with React Native </h2> </div>
+<div> <h5> ° For instaling dependences, npm install </h5> </div>
+<div> <h5> ° For init project go into the same folder, and run npm start </h5> </div>
---
+++ src/components/Title/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+
+export const Container = styled.Text`
+    padding: 9px 0;
+    color: ${colors.gray};
+    background: ${colors.primary};
+    font-size: 14px;
+    text-transform: uppercase;
+`;
